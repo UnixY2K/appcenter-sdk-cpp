61a54fceac5643806e1875b1eee0b7ce0d937aef
Jonathan Guzmán <jonguz5@hotmail.com>
2022-02-01 20:46:16 -0600

    add option to define app secret for demos

--- samples/demo/src/example.cpp
+++ samples/demo/src/example.cpp
@@ -3,7 +3,12 @@
 
 void initAppCenter() {
 	// dummy init app center
+	#ifdef APPCENTER_SAMPLE_APP_SECRET
+	std::string appSecret = APPCENTER_SAMPLE_APP_SECRET;
+	#else
+	// if not using meson, you can setup your own app secret here
 	std::string appSecret = "YOUR_APP_SECRET";
+	#endif
 	appcenter::Services_t services =
 	    appcenter::Services_t::analytics | appcenter::Services_t::crash | appcenter::Services_t::distribute;
 	appcenter::start(appSecret, services);
