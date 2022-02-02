a04b5cf98c5c163c54d3ecef437bdb3bdee1b15d
Jonathan Guzmán <jonguz5@hotmail.com>
2022-02-01 22:27:31 -0600

    sdk internal constants

---
+++ sdk/appcenter/private/appcenter/constants.hpp
@@ -0,0 +1,17 @@
+#pragma once
+#include <string_view>
+#include <appcenter/macros.hpp>
+
+
+namespace appcenter::constants{
+    constexpr std::string_view sdkName = "appcenter.native";
+    #ifdef APPCENTER_SDK_VERSION
+    constexpr std::string_view sdkVersion = APPCENTER_SDK_VERSION;
+    #else
+    constexpr std::string_view sdkVersion = "unknown";
+    #endif
+    // the native value of the wrapper SDK should be replaced by
+    // the detected platform, ex: "NativeCPP.Linux", or "NativeCPP.Windows"
+    constexpr std::string_view wrapperSdkVersion = "NativeCPP.Unknown/Generic";
+    constexpr std::string_view wrapperSdkName = "appcenter.NativeCPP";
+}
