6e86b1e0b1c1807e8bf233ce2f6aca8c22bbe69b
Jonathan Guzmán <jonguz5@hotmail.com>
2022-02-02 21:06:31 -0600

    add Device class

--- sdk/appcenter/private/appcenter/ingestion/device.hpp
+++ sdk/appcenter/private/appcenter/ingestion/device.hpp
@@ -3,6 +3,7 @@
 #include <appcenter/constants.hpp>
 #include <chrono>
 #include <string>
+#include <string_view>
 
 namespace appcenter::core::ingestion {
 /**
@@ -11,32 +12,88 @@ namespace appcenter::core::ingestion {
  * @note this class does not contain the app secret and install ID.
  *
  */
+using Timestamp = std::chrono::time_point<std::chrono::system_clock>;
 class Device {
 	// required data
 	std::string sdkName{constants::sdkName};
-    // sdk version
+	// sdk version
 	std::string sdkVersion{constants::sdkVersion};
-    // OS name, ex: "Windows-UWP", "Linux", "MacOSX""
+	// OS name, ex: "Windows-UWP", "Linux", "MacOSX""
 	std::string osName;
-    // OS version, ex: "Ubuntu (kernel 4.4.0-53-generic), 64-bit", "Windows 10", "Mac OSX 10.12.6"
+	// OS version, ex: "Ubuntu (kernel 4.4.0-53-generic), 64-bit", "Windows 10",
+	// "Mac OSX 10.12.6"
 	std::string osVersion;
-    // system locale, ex: "en-US"
+	// system locale, ex: "en-US"
 	std::string locale;
-	std::chrono::time_point<std::chrono::system_clock> timeZoneOffset;
+	Timestamp timeZoneOffset;
 	std::string appVersion;
 	std::string appBuild;
 	// non required data for ingestion
-	std::string timestamp;
 	std::string sid;
 	std::string userID;
-	std::string wrapperSdkVersion {constants::wrapperSdkVersion};
-	std::string wrapperSdkName {constants::wrapperSdkName};
+	std::string wrapperSdkVersion{constants::wrapperSdkVersion};
+	std::string wrapperSdkName{constants::wrapperSdkName};
 	std::string model;
 	std::string oemName;
 	std::string osBuild;
-    // -1 means unknown
+	// -1 means unknown
 	int osApiLevel = -1;
 	std::string screenSize;
 	std::string appNamespace;
+
+  public:
+	const std::string_view getSdkName() const;
+	void setSdkName(const std::string &sdkName);
+
+	const std::string_view getSdkVersion() const;
+	void setSdkVersion(const std::string &sdkVersion);
+
+	const std::string_view getOsName() const;
+	void setOsName(const std::string &osName);
+
+	const std::string_view getOsVersion() const;
+	void setOsVersion(const std::string &OsVersion);
+
+	const std::string_view getLocale() const;
+	void setLocale(const std::string &locale);
+
+	const Timestamp &getTimeZoneOffset() const;
+	void setTimeZoneOffset(const Timestamp &timeZoneOffset);
+
+	const std::string_view getAppVersion() const;
+	void setAppVersion(const std::string &appVersion);
+
+	const std::string_view getAppBuild() const;
+	void setAppBuild(const std::string &appBuild);
+
+	const std::string_view getSid() const;
+	void setSid(const std::string &sid);
+
+	const std::string_view getUserId() const;
+	void setUserID(const std::string &userID);
+
+	const std::string_view getWrapperSdkVersion() const;
+	void setWrapperSdkVersion(const std::string &wrapperSdkVersion);
+
+	const std::string_view getWrapperSdkName() const;
+	void setWrapperSdkName(const std::string &wrapperSdkName);
+
+	const std::string_view getModel() const;
+	void setModel(const std::string &model);
+
+	const std::string_view getOemName() const;
+	void setOemName(const std::string &oemName);
+
+	const std::string_view getOsBuild() const;
+	void setOsBuild(const std::string &osBuild);
+
+	const int getOsApiLevel() const;
+	void setOsApiLevel(const int osApiLevel);
+
+	const std::string_view getScreenSize() const;
+	void setScreenSize(const std::string &screenSize);
+
+	const std::string_view getAppNamespace() const;
+	void setAppNamespace(const std::string &appNamespace);
 };
 } // namespace appcenter::core::ingestion
---
+++ sdk/appcenter/src/appcenter/ingestion/device.cpp
@@ -0,0 +1,83 @@
+#include <appcenter/ingestion/device.hpp>
+
+namespace appcenter::core::ingestion {
+
+const std::string_view Device::getSdkName() const { return sdkName; }
+void Device::setSdkName(const std::string &sdkName) { this->sdkName = sdkName; }
+
+const std::string_view Device::getSdkVersion() const { return sdkVersion; }
+void Device::setSdkVersion(const std::string &sdkVersion) {
+	this->sdkVersion = sdkVersion;
+}
+
+const std::string_view Device::getOsName() const { return osName; }
+void Device::setOsName(const std::string &osName) { this->osName = osName; }
+
+const std::string_view Device::getOsVersion() const { return osVersion; }
+void Device::setOsVersion(const std::string &OsVersion) {
+	this->osVersion = OsVersion;
+}
+
+const std::string_view Device::getLocale() const { return locale; }
+void Device::setLocale(const std::string &locale) { this->locale = locale; }
+
+const Timestamp &Device::getTimeZoneOffset() const { return timeZoneOffset; }
+void Device::setTimeZoneOffset(const Timestamp &timeZoneOffset) {
+	this->timeZoneOffset = timeZoneOffset;
+}
+
+const std::string_view Device::getAppVersion() const { return appVersion; }
+void Device::setAppVersion(const std::string &appVersion) {
+	this->appVersion = appVersion;
+}
+
+const std::string_view Device::getAppBuild() const { return appBuild; }
+void Device::setAppBuild(const std::string &appBuild) {
+	this->appBuild = appBuild;
+}
+
+const std::string_view Device::getSid() const { return sid; }
+void Device::setSid(const std::string &sid) { this->sid = sid; }
+
+const std::string_view Device::getUserId() const { return userID; }
+void Device::setUserID(const std::string &userID) { this->userID = userID; }
+
+const std::string_view Device::getWrapperSdkVersion() const {
+	return wrapperSdkVersion;
+}
+void Device::setWrapperSdkVersion(const std::string &wrapperSdkVersion) {
+	this->wrapperSdkVersion = wrapperSdkVersion;
+}
+
+const std::string_view Device::getWrapperSdkName() const {
+	return wrapperSdkName;
+}
+void Device::setWrapperSdkName(const std::string &wrapperSdkName) {
+	this->wrapperSdkName = wrapperSdkName;
+}
+
+const std::string_view Device::getModel() const { return model; }
+void Device::setModel(const std::string &model) { this->model = model; }
+
+const std::string_view Device::getOemName() const { return oemName; }
+void Device::setOemName(const std::string &oemName) { this->oemName = oemName; }
+
+const std::string_view Device::getOsBuild() const { return osBuild; }
+void Device::setOsBuild(const std::string &osBuild) { this->osBuild = osBuild; }
+
+const int Device::getOsApiLevel() const { return osApiLevel; }
+void Device::setOsApiLevel(const int osApiLevel) {
+	this->osApiLevel = osApiLevel;
+}
+
+const std::string_view Device::getScreenSize() const { return screenSize; }
+void Device::setScreenSize(const std::string &screenSize) {
+	this->screenSize = screenSize;
+}
+
+const std::string_view Device::getAppNamespace() const { return appNamespace; }
+void Device::setAppNamespace(const std::string &appNamespace) {
+	this->appNamespace = appNamespace;
+}
+
+} // namespace appcenter::core::ingestion
