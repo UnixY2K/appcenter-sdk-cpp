f5ea01b2e29750cecd28ebbf80fb8e242e3f12e0
Jonathan Guzmán <jonguz5@hotmail.com>
2022-02-11 18:30:44 -0600

    use string view in macro definitions

--- sdk/appcenter/private/appcenter/sdk/constants.hpp
+++ sdk/appcenter/private/appcenter/sdk/constants.hpp
@@ -11,7 +11,10 @@ constexpr std::string_view sdkVersion = APPCENTER_SDK_VERSION;
 #else
 constexpr std::string_view sdkVersion = unknown_value;
 #endif
-constexpr std::string_view platform = APPCENTER_PLATFORM;
+constexpr std::string_view platform = macros::APPCENTER_PLATFORM;
+constexpr bool isUnix = macros::APPCENTER_PLATFORM == "Linux" ||
+                        macros::APPCENTER_PLATFORM == "MacOSX" ||
+                        macros::APPCENTER_PLATFORM == "BSD";
 // the native value of the wrapper SDK should be replaced by
 // the detected platform, ex: "NativeCPP.Linux", or "NativeCPP.Windows"
 constexpr std::string_view wrapperSdkVersion = "NativeCPP.Unknown/Generic";
--- sdk/appcenter/private/appcenter/sdk/utils/macros.hpp
+++ sdk/appcenter/private/appcenter/sdk/utils/macros.hpp
@@ -1,27 +1,44 @@
 #pragma once
+#include <string_view>
 
 // macros definitions for constants
 
+namespace appcenter::sdk::macros {
+constexpr std::string_view APPCENTER_GENERIC_PLATFORM = "Unknown/Generic";
+constexpr std::string_view APPCENTER_UWP_PLATFORM = "UWP";
+constexpr std::string_view APPCENTER_WINDOWS_PLATFORM = "Windows";
+constexpr std::string_view APPCENTER_LINUX_PLATFORM = "Linux";
+constexpr std::string_view APPCENTER_MACOS_PLATFORM = "MacOSX";
+// NOTE: BSD is not planned to be supported
+constexpr std::string_view APPCENTER_BSD_PLATFORM = "BSD";
+} // namespace appcenter::sdk::macros
 
-#define APPCENTER_GENERIC_PLATFORM "Unknown/Generic"
 // check if is uwp, windows, linux, macosx
 #if defined(WINAPI_FAMILY) && (WINAPI_FAMILY == WINAPI_FAMILY_APP)
 // Windows Store or Universal Windows Platform
-#define APPCENTER_PLATFORM "UWP"
+#define APPCENTER_MACRO_PLATFORM appcenter::sdk::macros::APPCENTER_UWP_PLATFORM
 #elif defined(_WIN32)
 // Windows Desktop
-#define APPCENTER_PLATFORM "Windows"
+#define APPCENTER_MACRO_PLATFORM                                               \
+	appcenter::sdk::macros::APPCENTER_WINDOWS_PLATFORM
 #elif defined(__linux__)
 // linux
-#define APPCENTER_PLATFORM "Linux"
+#define APPCENTER_MACRO_PLATFORM                                               \
+	appcenter::sdk::macros::APPCENTER_LINUX_PLATFORM
 #elif defined(__APPLE__) && defined(__MACH__)
 // Mac OSX
-#define APPCENTER_PLATFORM "MacOSX"
+#define APPCENTER_MACRO_PLATFORM                                               \
+	appcenter::sdk::macros::APPCENTER_MACOS_PLATFORM
 #elif defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__) ||   \
     defined(__bsdi__) || defined(__DragonFly__)
 // bsd
-#define APPCENTER_PLATFORM "BSD"
+#define APPCENTER_MACRO_PLATFORM appcenter::sdk::macros::APPCENTER_BSD_PLATFORM
 #else
 // unknown
-#define APPCENTER_PLATFORM APPCENTER_GENERIC_PLATFORM
+#define APPCENTER_MACRO_PLATFORM                                               \
+	appcenter::sdk::macros::APPCENTER_GENERIC_PLATFORM
 #endif
+
+namespace appcenter::sdk::macros {
+constexpr std::string_view APPCENTER_PLATFORM = APPCENTER_MACRO_PLATFORM;
+}
