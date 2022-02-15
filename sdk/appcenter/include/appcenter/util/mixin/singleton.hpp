2e17e92205e0141533ad6ef3c4034893236870fa
Jonathan Guzmán <jonguz5@hotmail.com>
2022-02-15 07:49:50 -0600

    fix singleton pattern

--- sdk/appcenter/include/appcenter/util/mixin/singleton.hpp
+++ sdk/appcenter/include/appcenter/util/mixin/singleton.hpp
@@ -7,6 +7,10 @@ template <class T> class Singleton {
 	Singleton() = default;
 
   public:
+	Singleton(const Singleton &) = delete;
+	Singleton &operator=(const Singleton &) = delete;
+	Singleton(Singleton &&) = delete;
+	Singleton &operator=(Singleton &&) = delete;
 	static T &getInstance() {
 		static T instance;
 		return instance;
