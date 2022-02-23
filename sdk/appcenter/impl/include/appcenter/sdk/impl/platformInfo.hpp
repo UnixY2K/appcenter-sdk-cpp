900de36f8b918ea5c3b5090f05be8c5100aa827a
Jonathan Guzmán <jonguz5@hotmail.com>
2022-02-23 17:53:30 -0600

    platform info definition

---
+++ sdk/appcenter/impl/include/appcenter/sdk/impl/platformInfo.hpp
@@ -0,0 +1,38 @@
+#pragma once
+#include <appcenter/sdk/constants.hpp>
+#include <memory>
+#include <string_view>
+
+namespace appcenter::sdk::impl {
+namespace platform {
+namespace constants {
+constexpr std::string_view unknown_value = "Unknown";
+constexpr std::string_view undefined_value = "Undefined";
+} // namespace constants
+
+class PlatformInfo {
+	struct platformImplData;
+	// Pimpl
+	std::unique_ptr<platformImplData> impl_info = nullptr;
+
+  public:
+	PlatformInfo();
+	PlatformInfo(const PlatformInfo &other);
+	PlatformInfo(PlatformInfo &&other) noexcept;
+
+	const std::string_view getModel() const;
+	const std::string_view getOemName() const;
+	const std::string_view getOsName() const;
+	const std::string_view getOsVersion() const;
+	const std::string_view getOsBuild() const;
+	const std::string_view getLocale() const;
+	const std::string_view getScreenResolution() const;
+
+	PlatformInfo &operator=(const PlatformInfo &other);
+	PlatformInfo &operator=(PlatformInfo &&other) noexcept;
+	~PlatformInfo();
+	static PlatformInfo getPlatformInfo();
+};
+} // namespace platform
+
+} // namespace appcenter::sdk::impl
